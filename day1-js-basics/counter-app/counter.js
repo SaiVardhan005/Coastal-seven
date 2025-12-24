f30e500c2a26f5e5913b1f2c5d603ff69fe66e83
SaiVardhan005 <saivardhan1502@gmail.com>
2025-12-24 11:48:38 +0530

    Day 1 completed: HTML, CSS, JS basics and Counter App

---
+++ day1-js-basics/counter-app/counter.js
@@ -0,0 +1,16 @@
+let count = 0;
+
+function inc() {
+  count++;
+  document.getElementById("count").innerText = count;
+}
+
+function dec() {
+  count--;
+  document.getElementById("count").innerText = count;
+}
+
+function reset() {
+  count = 0;
+  document.getElementById("count").innerText = count;
+}
---
+++ day1-js-basics/js-practice.js
@@ -0,0 +1,5 @@
+let arr = [1, 2, 3, 4];
+
+console.log(arr.map(n => n * 2));
+console.log(arr.filter(n => n % 2 === 0));
+console.log(arr.reduce((a, b) => a + b));
